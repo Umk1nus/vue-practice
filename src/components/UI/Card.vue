1437e4efcd453ce6464f8b036f3b505724b9ead4
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-03-19 21:34:24 +0300

    part 2

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{lvl}}</span>
+    <span class="card-title">{{title}}</span>
+    <div class="card-body">
+      {{description}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    lvl: {
+      type: String,
+      required: true,
+    },
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,49 @@
 <template>
-  <h1>Home</h1>
+  <div class="md-body">
+    <h1>Home</h1>
+    <div class="card_wrapper" v-for="card in cards" :key="card.id">
+      <Card :title="card.title" :description="card.description" :lvl="`${card.lvl}`"/>
+    </div>
+  </div>
 </template>
 
+<script>
+import Card from '@/components/UI/Card'
+export default {
+  components:{Card},
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          title: 'archer',
+          description: 'archer archer archer',
+          lvl: 4
+        },
+        {
+          id: 2,
+          title: 'barbarian',
+          description: 'barbarian barbarian barbarian',
+          lvl: 5
+        },
+        {
+          id: 3,
+          title: 'giant',
+          description: 'giant giant giant',
+          lvl: 1
+        },
+        {
+          id: 4,
+          title: 'wizard',
+          description: 'wizard wizard wizard',
+          lvl: 8
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
